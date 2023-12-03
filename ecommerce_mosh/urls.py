cbf86378741bf52844023614cf41d4e93fc57e61
Amr Elaraby <Amr.Elaraby@scctportsaid.com>
2023-12-03 17:00:03 +0200

    finished related queries

--- ecommerce_mosh/urls.py
+++ ecommerce_mosh/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
+from playground import views
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("playground/", include("playground.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
+    path("api/", views.app.urls),
 ]
--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.db.models import Q, F
+from django.core.exceptions import ObjectDoesNotExist
+from store.models import Product, OrderItem, Order
+from ninja import NinjaAPI
+
+app = NinjaAPI()
 
 # Create your views here.
 
 
 def say_hello(request):
-    return render(request, "hello.html", {})
+    query_set = (
+        Order.objects.select_related("customer")
+        .prefetch_related("orderitem_set__product")
+        .order_by(
+            "-placed_at",
+        )[:5]
+    )
+
+    return render(request, "hello.html", {"query_set": list(query_set)})
