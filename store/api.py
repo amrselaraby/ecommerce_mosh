9008e1edbaa54b9b196ad231e2f59bab21a4687c
Amr Elaraby <amrselaraby@gmail.com>
2023-12-13 05:23:59 +0200

    13Dec dawn

--- store/api.py
+++ store/api.py
@@ -1,14 +1,19 @@
 from ninja import Router
+from django.shortcuts import get_object_or_404
 from .models import Product
+from .schema import ProductBase
+from typing import List
 
 router = Router()
 
 
-@router.get("/products/")
+@router.get("/products/", response=List[ProductBase])
 def product_list(request):
-    return {"hello": "world"}
+    products = Product.objects.all()
+    return products
 
 
-@router.get("/products/{product_id}")
+@router.get("/products/{product_id}", response=ProductBase)
 def product_detail(request, product_id: int):
-    return {"product_id": product_id}
+    product = get_object_or_404(Product, pk=product_id)
+    return product
