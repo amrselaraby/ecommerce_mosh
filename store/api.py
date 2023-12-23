25957bb3bf8c6e02e1e3f29975f1703345bdeb29
Amr Elaraby <amrselaraby@gmail.com>
2023-12-23 17:27:34 +0200

    have to build auth manual

--- store/api.py
+++ store/api.py
@@ -1,11 +1,17 @@
+from uuid import uuid4
 from ninja import Path, Query, Router
 from django.shortcuts import get_object_or_404
 from django.db.models.aggregates import Count
-from .models import Product, Collection, Review
+from .models import Cart, CartItem, Product, Collection, Review
 from .schemas import (
+    CartBase,
+    CartItemBase,
+    CartItemIn,
+    CartOut,
     CollectionBase,
     CollectionIn,
     CollectionOut,
+    MsgError,
     ProductBase,
     ProductFilterSchema,
     ProductIn,
@@ -17,6 +23,8 @@ from typing import List
 router = Router()
 review_router = Router(tags=["reviews"])
 product_router = Router(tags=["products"])
+cart_router = Router(tags=["carts"])
+cart_item_router = Router(tags=["cart_items"])
 
 
 @product_router.get("/", response=List[ProductBase])
@@ -53,7 +61,7 @@ def product_update(request, product_id: int, payload: ProductIn):
 
 
 @product_router.delete("/{product_id}")
-def product_update(request, product_id: int, payload: ProductIn):
+def product_delete(request, product_id: int, payload: ProductIn):
     product = get_object_or_404(Product, pk=product_id)
     if product.orderitems.count() > 0:
         return {405: "Method Not Allowed"}
@@ -68,13 +76,13 @@ def collection_list(request):
 
 
 @router.post("/collections/", response={201: CollectionBase})
-def collection_list(request, payload: CollectionIn):
+def collection_create(request, payload: CollectionIn):
     collection = Collection.objects.create(**payload.dict())
     return collection
 
 
 @router.get("/collections/{collection_id}", response=CollectionOut)
-def collection_list(request, collection_id: int):
+def collection_detail(request, collection_id: int):
     collection = get_object_or_404(
         Collection.objects.annotate(products_count=Count("products")), pk=collection_id
     )
@@ -91,7 +99,7 @@ def collection_update(request, collection_id: int, payload: CollectionIn):
 
 
 @router.delete("/collections/{collection_id}")
-def collection_update(request, collection_id: int):
+def collection_delete(request, collection_id: int):
     collection = get_object_or_404(Collection, pk=collection_id)
     if collection.products.count() > 0:
         return {405: "Method Not Allowed"}
@@ -147,5 +155,91 @@ def review_delete(request, review_id: int, product_id: int = Path(...)):
     return review
 
 
+@cart_router.post("/", response={201: CartBase})
+def cart_create(request):
+    cart = Cart.objects.create()
+    return cart
+
+
+@cart_router.get("/{uuid:cart_id}", response={200: CartOut})
+def cart_details(request, cart_id):
+    cart = get_object_or_404(
+        Cart.objects.prefetch_related("items__product").all(), pk=cart_id
+    )
+    return cart
+
+
+@cart_router.delete("/{uuid:cart_id}", response={204: None})
+def cart_delete(request, cart_id):
+    cart = get_object_or_404(Cart, pk=cart_id)
+    cart.delete()
+    return cart
+
+
+@cart_item_router.get("/", response=List[CartItemBase])
+def cart_items_list(request, cart_id=Path(...)):
+    items = (
+        CartItem.objects.select_related("cart")
+        .select_related("product")
+        .filter(cart_id=cart_id)
+    )
+
+    return items
+
+
+@cart_item_router.get("/{item_id}", response=CartItemBase)
+def cart_items_details(request, item_id: int, cart_id=Path(...)):
+    item = get_object_or_404(
+        CartItem.objects.select_related("cart")
+        .select_related("product")
+        .filter(cart_id=cart_id),
+        pk=item_id,
+    )
+
+    return item
+
+
+@cart_item_router.post("/", response={201: CartItemBase, 404: MsgError})
+def cart_items_create(request, payload: CartItemIn, cart_id=Path(...)):
+    if not Product.objects.filter(pk=payload.product_id).exists():
+        return 404, {"msg": "Product does not exist"}
+    try:
+        item = CartItem.objects.get(cart_id=cart_id, product_id=payload.product_id)
+        item.quantity += payload.quantity
+        item.save()
+    except CartItem.DoesNotExist:
+        item = CartItem.objects.create(cart_id=cart_id, **payload.dict())
+
+    return 201, item
+
+
+@cart_item_router.patch("/", response={200: CartItemBase, 404: MsgError})
+def cart_items_update(request, payload: CartItemIn, cart_id=Path(...)):
+    if not Product.objects.filter(pk=payload.product_id).exists():
+        return 404, {"msg": "Product does not exist"}
+
+    item = CartItem.objects.get(cart_id=cart_id, product_id=payload.product_id)
+    item.quantity = payload.quantity
+    item.save()
+
+    return 200, item
+
+
+@cart_item_router.delete("/{item_id}", response={204: None})
+def cart_items_delete(request, item_id: int, cart_id=Path(...)):
+    item = get_object_or_404(
+        CartItem.objects.select_related("cart")
+        .select_related("product")
+        .filter(cart_id=cart_id),
+        pk=item_id,
+    )
+
+    item.delete()
+
+    return 204, None
+
+
+cart_router.add_router("{uuid:cart_id}/items", cart_item_router)
 product_router.add_router("{product_id}/reviews", review_router)
 router.add_router("/products", product_router)
+router.add_router("/carts", cart_router)
--- store/schemas.py
+++ store/schemas.py
@@ -1,7 +1,8 @@
 from ninja import Field, Schema, ModelSchema, FilterSchema
 from decimal import Decimal
 from typing import List, Optional
-from .models import Collection, Product, Review
+from .models import CartItem, Collection, Product, Review
+from uuid import UUID, uuid4
 
 
 class ProductFilterSchema(FilterSchema):
@@ -45,6 +46,12 @@ class ProductBase(ModelSchema):
         return product.unit_price * Decimal(1.1)
 
 
+class ProductOut(ModelSchema):
+    class Meta:
+        model = Product
+        fields = ["id", "title", "unit_price"]
+
+
 class ProductIn(Schema):
     title: str
     slug: str
@@ -62,3 +69,40 @@ class ReviewBase(ModelSchema):
 class ReviewIn(Schema):
     name: str
     description: str
+
+
+class CartItemBase(ModelSchema):
+    product: ProductOut
+    total_price: Decimal
+
+    class Meta:
+        model = CartItem
+        fields = ["id", "quantity"]
+
+    @staticmethod
+    def resolve_total_price(cart_item):
+        return cart_item.quantity * cart_item.product.unit_price
+
+
+class CartBase(Schema):
+    id: UUID = Field(default_factory=uuid4)
+
+
+class CartOut(CartBase):
+    items: List[CartItemBase]
+    total_price: Decimal
+
+    @staticmethod
+    def resolve_total_price(cart):
+        return sum(
+            [item.quantity * item.product.unit_price for item in cart.items.all()]
+        )
+
+
+class CartItemIn(Schema):
+    product_id: int = Field(gt=0)
+    quantity: int = Field(ge=0)
+
+
+class MsgError(Schema):
+    msg: str
